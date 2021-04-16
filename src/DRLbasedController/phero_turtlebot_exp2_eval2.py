1030858577d64844ebdfd26eb6cbf5cbb66a459c
miyembe <respct11@gmail.com>
2021-04-16 14:16:16 +0900

    eval added

--- src/DRLbasedController/phero_turtlebot_exp2_eval2.py
+++ src/DRLbasedController/phero_turtlebot_exp2_eval2.py
@@ -346,7 +346,7 @@ class Env:
         if (self.counter_step % 1 == 0 and self.counter_step != 0):
             print("Success Rate: {}%".format(succ_percentage))
 
-        if (self.counter_step % 1 == 0 and self.counter_step != 0):
+        if (self.counter_step % 100 == 0 and self.counter_step != 0):
             avg_comp = np.average(np.asarray(self.arrival_time))
             std_comp = np.std(np.asarray(self.arrival_time))
             print("{} trials ended. Success rate: {}, average completion time: {}, Standard deviation: {}, Collision rate: {}, Timeout Rate: {}".format(self.counter_step, succ_percentage, avg_comp, std_comp, col_percentage, tout_percentage))
